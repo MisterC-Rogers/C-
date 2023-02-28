f125c138156d5b157e11306860f5e81cd95e56ae
MisterC-Rogers <charles.rogers2024@gmail.com>
2023-02-28 10:14:03 -0600

    first commit

---
+++ shortBioChallenge/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Charlie Rogers");
+Console.WriteLine("I am a father, husband, and a Vet");
+Console.WriteLine("I love to code, make music, and play video games");
+Console.WriteLine("It is very nice to meet you");
