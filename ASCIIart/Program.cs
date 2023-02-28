3fd890a742ec98ee7d9b11bbbbea46534ab19276
MisterC-Rogers <charles.rogers2024@gmail.com>
2023-02-28 11:38:49 -0600

    ascii art

---
+++ ASCIIart/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Red;
+// Console.WriteLine(" ^..^____/");
+// Console.WriteLine(" `<  ___ )");
+// Console.WriteLine("   ||  || mh");
+
+/*
+| Color Code | Dark Color Code |
+|------------|-----------------|
+ConsoleColor.White | ConsoleColor.Black
+ConsoleColor.Gray | ConsoleColor.DarkGray
+ConsoleColor.Red | ConsoleColor.DarkRed
+ConsoleColor.Blue | ConsoleColor.DarkBlue
+ConsoleColor.Green | ConsoleColor.DarkGreen
+ConsoleColor.Cyan | ConsoleColor.DarkCyan
+ConsoleColor.Magenta | ConsoleColor.DarkMagenta
+ConsoleColor.Yellow | ConsoleColor.DarkYellow
+*/
+
+Console.WriteLine("    /                       \\");
+Console.WriteLine("  /X/                       \\X\\");
+Console.WriteLine(" |XX\\         _____         /XX|");
+Console.WriteLine(" |XXX\\    _ /       \\ _    /XXX|");
+Console.WriteLine(" \\XXXXXXX               XXXXXXX/");
+Console.WriteLine("    \\XXXX    /     \\    XXXXX/");
+Console.WriteLine("         |   0     0   |");
+Console.WriteLine("          |           |");
+Console.WriteLine("           \\         /");
+Console.WriteLine("            \\       /");
+Console.WriteLine("      Moo -  | O_O |");
+Console.WriteLine("              \\ _ / ");
+
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("       CCCCCCCCCCCCC   hhhhhh                                                 lllllll   iiii                           FFFFFFFFFFFFFFFFFFFFFFNNNNNNNN        NNNNNNNN     RRRRRRRRRRRRRRRRR                                                                                                 ");
+Console.WriteLine("     CCC::::::::::::C  h::::h                                                 l:::::l  i::::i                          F::::::::::::::::::::FN:::::::N       N::::::N     R::::::::::::::::R                                                                                                ");
+Console.WriteLine("    CC:::::::::::::::C h::::h                                                 l:::::l   iiii                           F::::::::::::::::::::FN::::::::N      N::::::N     R::::::RRRRRR:::::R                                                                                               ");
+Console.WriteLine("   C:::::CCCCCCCC::::C h::::h                                                 l:::::l                                  FF::::::FFFFFFFFF::::FN:::::::::N     N::::::N     RR:::::R     R:::::R                                                                                              ");
+Console.WriteLine("  C:::::C       CCCCCC h::::h hhhhh         aaaaaaaaaaaaa  rrrrr   rrrrrrrrr   l::::l iiiiiii     eeeeeeeeeeee           F:::::F       FFFFFFN::::::::::N    N::::::N       R::::R     R:::::R   ooooooooooo      ggggggggg   ggggg    eeeeeeeeeeee    rrrrr   rrrrrrrrr       ssssssssss   ");
+Console.WriteLine(" C:::::C               h::::hh:::::hhh      a::::::::::::a r::::rrr:::::::::r  l::::l i:::::i   ee::::::::::::ee         F:::::F             N:::::::::::N   N::::::N       R::::R     R:::::R oo:::::::::::oo   g:::::::::ggg::::g  ee::::::::::::ee  r::::rrr:::::::::r    ss::::::::::s  ");
+Console.WriteLine(" C:::::C               h::::::::::::::hh    aaaaaaaaa:::::ar:::::::::::::::::r l::::l  i::::i  e::::::eeeee:::::ee       F::::::FFFFFFFFFF   N:::::::N::::N  N::::::N       R::::RRRRRR:::::R o:::::::::::::::o g:::::::::::::::::g e::::::eeeee:::::eer:::::::::::::::::r ss:::::::::::::s ");
+Console.WriteLine(" C:::::C               h:::::::hhh::::::h            a::::arr::::::rrrrr::::::rl::::l  i::::i e::::::e     e:::::e       F:::::::::::::::F   N::::::N N::::N N::::::N       R:::::::::::::RR  o:::::ooooo:::::og::::::ggggg::::::gge::::::e     e:::::err::::::rrrrr::::::rs::::::ssss:::::s");
+Console.WriteLine(" C:::::C               h::::::h   h::::::h    aaaaaaa:::::a r:::::r     r:::::rl::::l  i::::i e:::::::eeeee::::::e       F:::::::::::::::F   N::::::N  N::::N:::::::N       R::::RRRRRR:::::R o::::o     o::::og:::::g     g:::::g e:::::::eeeee::::::e r:::::r     r:::::r s:::::s  ssssss ");
+Console.WriteLine(" C:::::C               h:::::h     h:::::h  aa::::::::::::a r:::::r     rrrrrrrl::::l  i::::i e:::::::::::::::::e        F::::::FFFFFFFFFF   N::::::N   N:::::::::::N       R::::R     R:::::Ro::::o     o::::og:::::g     g:::::g e:::::::::::::::::e  r:::::r     rrrrrrr   s::::::s      ");
+Console.WriteLine(" C:::::C               h:::::h     h:::::h a::::aaaa::::::a r:::::r            l::::l  i::::i e::::::eeeeeeeeeee         F:::::F             N::::::N    N::::::::::N       R::::R     R:::::Ro::::o     o::::og:::::g     g:::::g e::::::eeeeeeeeeee   r:::::r                  s::::::s   ");
+Console.WriteLine("  C:::::C       CCCCCC h:::::h     h:::::ha::::a    a:::::a r:::::r            l::::l  i::::i e:::::::e                  F:::::F             N::::::N     N:::::::::N       R::::R     R:::::Ro::::o     o::::og::::::g    g:::::g e:::::::e            r:::::r            ssssss   s:::::s ");
+Console.WriteLine("   C:::::CCCCCCCC::::C h:::::h     h:::::ha::::a    a:::::a r:::::r           l::::::li::::::ie::::::::e               FF:::::::FF           N::::::N      N::::::::N     RR:::::R     R:::::Ro:::::ooooo:::::og:::::::ggggg:::::g e::::::::e           r:::::r            s:::::ssss::::::s");
+Console.WriteLine("    CC:::::::::::::::C h:::::h     h:::::ha:::::aaaa::::::a r:::::r           l::::::li::::::i e::::::::eeeeeeee       F::::::::FF           N::::::N       N:::::::N     R::::::R     R:::::Ro:::::::::::::::o g::::::::::::::::g  e::::::::eeeeeeee   r:::::r            s::::::::::::::s ");
+Console.WriteLine("      CCC::::::::::::C h:::::h     h:::::h a::::::::::aa:::ar:::::r           l::::::li::::::i  ee:::::::::::::e       F::::::::FF           N::::::N        N::::::N     R::::::R     R:::::R oo:::::::::::oo   gg::::::::::::::g   ee:::::::::::::e   r:::::r             s:::::::::::ss  ");
+Console.WriteLine("         CCCCCCCCCCCCC hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaarrrrrrr           lllllllliiiiiiii    eeeeeeeeeeeeee       FFFFFFFFFFF           NNNNNNNN         NNNNNNN     RRRRRRRR     RRRRRRR   ooooooooooo       gggggggg::::::g     eeeeeeeeeeeeee   rrrrrrr              sssssssssss    ");
+Console.WriteLine("                                                                                                                                                                                                                           g:::::g                                                          ");
+Console.WriteLine("                                                                                                                                                                                                               gggggg      g:::::g                                                          ");
+Console.WriteLine("                                                                                                                                                                                                               g:::::gg   gg:::::g                                                          ");
+Console.WriteLine("                                                                                                                                                                                                                g::::::ggg:::::::g                                                          ");
+Console.WriteLine("                                                                                                                                                                                                                 gg:::::::::::::g                                                           ");
+Console.WriteLine("                                                                                                                                                                                                                   ggg::::::ggg                                                             ");
+Console.WriteLine("                                                                                                                                                                                                                      gggggg                                                                ");
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(" $$$$$$\\  $$\\                           $$\\ $$\\                 $$$$$$$$\\ $$\\   $$\\       $$$$$$$\\                                                    ");
+Console.WriteLine("$$  __$$\\ $$ |                          $$ |\\__|                $$  _____|$$$\\  $$ |      $$  __$$\\                                                   ");
+Console.WriteLine("$$ /  \\__|$$$$$$$\\   $$$$$$\\   $$$$$$\\  $$ |$$\\  $$$$$$\\        $$ |      $$$$\\ $$ |      $$ |  $$ | $$$$$$\\   $$$$$$\\   $$$$$$\\   $$$$$$\\   $$$$$$$\\ ");
+Console.WriteLine("$$ |      $$  __$$\\  \\____$$\\ $$  __$$\\ $$ |$$ |$$  __$$\\       $$$$$\\    $$ $$\\$$ |      $$$$$$$  |$$  __$$\\ $$  __$$\\ $$  __$$\\ $$  __$$\\ $$  _____|");
+Console.WriteLine("$$ |      $$ |  $$ | $$$$$$$ |$$ |  \\__|$$ |$$ |$$$$$$$$ |      $$  __|   $$ \\$$$$ |      $$  __$$< $$ /  $$ |$$ /  $$ |$$$$$$$$ |$$ |  \\__|\\$$$$$$\\  ");
+Console.WriteLine("$$ |  $$\\ $$ |  $$ |$$  __$$ |$$ |      $$ |$$ |$$   ____|      $$ |      $$ |\\$$$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|$$ |       \\____$$\\ ");
+Console.WriteLine("\\$$$$$$  |$$ |  $$ |\\$$$$$$$ |$$ |      $$ |$$ |\\$$$$$$$\\       $$ |      $$ | \\$$ |      $$ |  $$ |\\$$$$$$  |\\$$$$$$$ |\\$$$$$$$\\ $$ |      $$$$$$$  |");
+Console.WriteLine(" \\______/ \\__|  \\__| \\_______|\\__|      \\__|\\__| \\_______|      \\__|      \\__|  \\__|      \\__|  \\__| \\______/  \\____$$ | \\_______|\\__|      \\_______/ ");
+Console.WriteLine("                                                                                                              $$\\   $$ |                              ");
+Console.WriteLine("                                                                                                              \\$$$$$$  |                              ");
+Console.WriteLine("                                                                                                               \\______/                               ");
